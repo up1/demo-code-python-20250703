d87ae7abe0b7a46145cb7b5f23db8a31df595f3f
up1 <somkiat.p@gmail.com>
2025-07-04 15:01:37 +0700

    Demo with module

---
+++ demo_module/module1.py
@@ -0,0 +1,4 @@
+# from module2 import sayHi
+import module2
+module2.sayHi()
+module2.sayBye()
---
+++ demo_module/module2.py
@@ -0,0 +1,6 @@
+def sayHi():
+    print("Hello from module2!")
+
+
+def sayBye():
+    print("Goodbye from module2!")
